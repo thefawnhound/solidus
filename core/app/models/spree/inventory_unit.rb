5de6d6f2277ec65ecfdcc8347f94481df1e3d41f
Martin Meyerhoff <martin@stembolt.com>
2017-10-04 20:55:54 +0200

    Delegate order to shipment in Inventory Unit
    We don't need the ActiveRecord association here,
    and we still get the order from the shipment with this
    change.

--- core/app/models/spree/inventory_unit.rb
+++ core/app/models/spree/inventory_unit.rb
@@ -15,9 +15,8 @@ module Spree
     has_many :return_items, inverse_of: :inventory_unit, dependent: :destroy
     has_one :original_return_item, class_name: "Spree::ReturnItem", foreign_key: :exchange_inventory_unit_id, dependent: :destroy
     has_one :unit_cancel, class_name: "Spree::UnitCancel"
-    has_one :order, through: :shipment
 
-    delegate :order_id, to: :shipment
+    delegate :order, :order_id, to: :shipment, allow_nil: true
 
     def order=(_)
       raise "The order association has been removed from InventoryUnit. The order is now determined from the shipment."
